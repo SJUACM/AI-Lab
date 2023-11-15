f8d0cad1a35e3b3f487613dec2b1aa6467453c07
Faizan Ahmed <43652410+faizancodes@users.noreply.github.com>
2023-11-14 23:22:42 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import requests
 import discord
-from dotenv import load_dotenv
 import os
 from discord import app_commands
 from discord.ext import commands
@@ -10,36 +9,49 @@ import openai
 from PIL import Image
 import csv
 import aiohttp
-import numpy as np
-from transformers import BlipProcessor, BlipForConditionalGeneration
 import base64
-load_dotenv()
 
 
+# Initialize Discord bot intents with default settings, including basic events like join/leave.
 intents = discord.Intents.default()
+
+# Enable the bot to read message content, necessary for responding to user messages.
 intents.message_content = True
+
+# Create a bot instance with a command prefix '!', ready to handle commands and intents.
 client = commands.Bot(command_prefix="!", intents=intents)
 
 
 
-@client.tree.command(name='resume_score', description='See how well your resume matches with different job positions')
-@app_commands.describe(resume_link = "Google Drive Link to your Resume")
-@app_commands.checks.cooldown(1, 60, key=lambda i: (i.guild_id, i.user.id))
-async def resume_score(interaction: discord.Interaction, resume_link: str):
+# This is a slash command that calls the GPT-3 API to generate a response and sends it back to the user on discord
+@client.tree.command(name='query_gpt', description='Ask GPT a question')
+@app_commands.describe(query = "Ask GPT a question")
+@app_commands.checks.cooldown(1, 60, key=lambda i: (i.guild_id, i.user.id)) # This prevents people from spamming the command. They can only use it once every 60 seconds.
+async def query_gpt(interaction: discord.Interaction, query: str):
     
-    return 
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": query}
+        ],
+    )
+    
+    response = completion.choices[0].message.content
+    await interaction.response.send_message(f"{response}")
+    
 
+# Helper function to encode images to base64.
+# Used to send images to the GPT-4 Vision API.
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 
-
-# Create a coroutine that handles image messages.
+# How to generate memes from images.
+# This sends a meme caption to the user on discord when they upload an image and type "memeify" in the message.
 @client.event
 async def on_message(message):
-  
-  global headers
   
   if message.author == client.user:
         return
@@ -52,6 +64,10 @@ async def on_message(message):
         async with session.get(image_url) as response:
             if response.status == 200:
                 
+                headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer <OPENAI_API_KEY>" # Replace this with your OpenAI API key.
+                }
 
                 payload = {
                     "model": "gpt-4-vision-preview",
@@ -87,6 +103,7 @@ async def on_message(message):
 
 
 
+
 @client.tree.error
 async def on_app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
     if isinstance(error, app_commands.CommandOnCooldown):
@@ -95,4 +112,6 @@ async def on_app_command_error(interaction: discord.Interaction, error: app_comm
         await interaction.response.send_message(f"Please wait for {round(error.retry_after, 0)} seconds before running the command again!", ephemeral=True)
 
 
+# This is the code that runs the bot.
+# Make sure to replace the DISCORD_TOKEN with your bot's token.
 client.run(os.environ.get('DISCORD_TOKEN'))
